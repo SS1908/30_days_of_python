666c6e77ebd36f46582c869893203be0ec015977
Sagar Chodavadiya <51920128+Rimmypatel@users.noreply.github.com>
2020-04-02 23:00:56 +0530

    Add files via upload

---
+++ Day 6/Difference_of_two_set.py
@@ -0,0 +1,16 @@
+#Two ways of difference
+# - operator
+# difference() method
+
+print("- operator")
+set1 = {"a","b","c","d"}
+set2 = {"c","d"}
+print(set1-set2)
+print()
+
+print("difference() method")
+set3 = {"a","b","c","d"}
+set4 = {"c","d"}
+print(set3.difference(set4))
+
+
