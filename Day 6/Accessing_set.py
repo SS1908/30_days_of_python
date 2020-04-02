37b19b7e95195ceb95ef0ae506a057c680ec0fb1
Sagar Chodavadiya <51920128+Rimmypatel@users.noreply.github.com>
2020-04-02 23:00:08 +0530

    initial commit

---
+++ Day 6/Accessing_set.py
@@ -0,0 +1,3 @@
+name = {"sagar","meet","jt","nayan","dallu"}
+for x in name :
+    print(x)
---
+++ Day 6/Adding _item_in_set.py
@@ -0,0 +1,11 @@
+#add()
+#update()
+
+name = {"sagar","nayan","meet","jt"}
+name.add("dallu")
+print(name)
+print()
+
+print("update method,With update method you can add more than one item")
+name.update(["pankti","haddi"])
+print(name)
