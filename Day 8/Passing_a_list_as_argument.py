2da164865a5aca8c76419386a7c84ed99548678c
Sagar Chodavadiya <51920128+Rimmypatel@users.noreply.github.com>
2020-04-04 23:21:04 +0530

    Function completed

---
+++ Day 8/Passing_a_list_as_argument.py
@@ -0,0 +1,6 @@
+def function1(frd):
+    for x in frd:
+        print(x)
+
+frd_list=["mee","jt","nayan","dallu"]
+function1(frd_list)
---
+++ Day 8/Returning_value.py
@@ -0,0 +1,6 @@
+def cube(a):
+    return a**3;
+
+print(cube(2))
+print(cube(3))
+print(cube(4))
