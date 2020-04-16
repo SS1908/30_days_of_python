afda9163999d128758519d9c645cbf5afae44c30
Sagar Chodavadiya <51920128+Rimmypatel@users.noreply.github.com>
2020-04-16 18:44:55 +0530

    map function

---
+++ Day 13/map_function.py
@@ -0,0 +1,12 @@
+num = [5,87,3,46,14,89,66,7]
+
+#TODO find a even number from the list
+even =list( filter(lambda a : a%2==0,num))
+print(even)
+print()
+
+#TODO double all the even number
+
+double = list(map(lambda n : n*2,even))
+print(double)
+print()
