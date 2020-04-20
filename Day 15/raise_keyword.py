388b6ba4f7b2d37fd0202195048d34790d7de673
Sagar Chodavadiya <51920128+Rimmypatel@users.noreply.github.com>
2020-04-20 15:19:51 +0530

    Exception Handling

---
+++ Day 15/raise_keyword.py
@@ -0,0 +1,10 @@
+"""
+    As a Python developer you can choose to throw an exception if a condition occurs.
+
+    To throw (or raise) an exception, use the raise keyword.
+"""
+
+x = -1
+
+if x<0:
+    raise Exception("sorry! Number below zero is not excepted")
